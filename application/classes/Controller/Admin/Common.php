5e68a8c0207a40d79340cef8999cd9089a6989cc
Alexander Vetlugin <vetlugins@gmail.com>
2017-03-08 23:14:43 +0300

    fix

--- application/classes/Controller/Admin/Common.php
+++ application/classes/Controller/Admin/Common.php
@@ -24,7 +24,8 @@ abstract class Controller_Admin_Common extends Controller_Template {
             HTTP::redirect('/');
         }
 
-        I18n::lang('ru');
+        if($_GET['lang']) I18n::lang($_GET['lang']);
+        else I18n::lang('ru');        
 
         /*Основные настройки сайта*/
         $this->params = array(
